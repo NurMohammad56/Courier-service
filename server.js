791ef4de59a800fe3d15e8e148ddf435da82cf26
NurMohammad56 <milonsardar56@gmail.com>
2025-05-07 12:50:41 +0600

    feat: Add WebSocket support and liveCoordinates to product model

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { Server } from 'socket.io'
 import dbConnection from './config/dbConnection.js';
 import globalErrorHandler from './src/middlewares/globalError.middlewares.js';
 import notFound from './src/middlewares/notFound.middlewares.js';
@@ -8,6 +9,8 @@ import routes from './config/route.config.js';
 const PORT = process.env.PORT || 5001;
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 dotenv.config({ path: './.env' });
 
@@ -18,23 +21,42 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use(globalErrorHandler);
-app.use(notFound);
+// WebSocket connection
+io.on('connection', (socket) => {
+    console.log('A client connected:', socket.id);
 
-app.get("/", (req, res) => {
-    return res.status(200).json({
-        status : true,
-        message : "Welcome to the server"
+    socket.on('joinProductRoom', (productId) => {
+        socket.join(productId); 
+        console.log(`Client joined room: ${productId}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
     });
 });
 
+// root route
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        status: true,
+        message: "Welcome to the server"
+    });
+});
+
+// routes
 app.use("/api/v1", routes);
 
-app.listen(PORT, async() => {
+// error handling middlewares
+app.use(globalErrorHandler);
+app.use(notFound);
+
+app.listen(PORT, async () => {
     await dbConnection();
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
+
+export { io }
 
 
 
